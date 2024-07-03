f13d4c4c9215bb180b3e9fd5f1ff9568a7b6dfcd
Alexander Lazarov <alexander.lazaroff@gmail.com>
2024-07-03 12:39:43 +0300

    Set neovide cursor to railgun

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -1,5 +1,7 @@
 if vim.g.neovide then
   -- thuis adds the path to volta and rbenv to the PATH
   vim.fn.setenv('PATH', os.getenv 'HOME' .. '/.volta/bin:' .. os.getenv 'HOME' .. '/.rbenv/shims:' .. os.getenv 'PATH')
+
+  vim.g.neovide_cursor_vfx_mode = 'railgun'
 end
 -- vim: ts=2 sts=2 sw=2 et
