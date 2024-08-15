3282fd8d207a8242110a0d7ecdc6f22729ef245f
Alexander Lazarov <alexander.lazaroff@gmail.com>
2024-08-15 10:30:44 +0300

    Change vim-test strategy
    This fixes colours in test output in neovide

--- lua/custom/plugins/vim-test.lua
+++ lua/custom/plugins/vim-test.lua
@@ -1,6 +1,7 @@
 return {
   'vim-test/vim-test',
   config = function()
+    vim.g['test#strategy'] = 'neovim'
     vim.keymap.set('n', '<leader>t', ':TestNearest<CR>')
     vim.keymap.set('n', '<leader>T', ':TestFile<CR>')
   end,
